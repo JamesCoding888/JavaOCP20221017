f66f0c8fc850c5d5bdd6b5e5052eec0fb7f6201a
James <JamesCoding888@hotmail.com>
2022-11-19 18:49:04 +0800

    day8

--- JavaOCP20221017/src/main/java/com/ocp/day2/bar/Apple.java
+++ JavaOCP20221017/src/main/java/com/ocp/day2/bar/Apple.java
@@ -6,10 +6,34 @@ public class Apple {
 	public static void a1() {
         System.out.println("a1()");
     }
+	
     // Visibility: default
     static void a2() {
         System.out.println("a2()");
     }
+
+    
+
+    /*     	
+     	The type of radius variable by "protected":
+    	把變數宣告為 protected 之用處:
+
+		1) This protected variable could be used mutually 
+		   父子類別皆能使用
+		   
+		2) Any .class is Out of the Parent and child's class, 
+		   they are NOT able to access into protected variable
+		   任何類別與父、子類別無繼承關係者，無法進入 protected variable 存取
+
+    */
+    // Visibility: protected
+    protected static double pi = Math.PI;
+    protected double radius;
+    public void show()
+    {
+       System.out.println("Area of Apple = " + pi * Math.pow(radius, 2));
+    }
+    
     // Visibility: private
     private static void a3() {
         System.out.println("a3()");
--- JavaOCP20221017/src/main/java/com/ocp/day8/WrapperClass1.java
+++ JavaOCP20221017/src/main/java/com/ocp/day8/WrapperClass1.java
@@ -1,5 +1,16 @@
 package com.ocp.day8;
 
 public class WrapperClass1 {
-
+    public static void main(String[] args) {
+        // 要印出 int 的最大值 
+        int x = Integer.MAX_VALUE;
+        System.out.println(x);
+        String a = "100"; // 國文成績
+        String b = "90";  // 數學成績
+        System.out.println(a + b); // 總分
+        // Integer.parseInt(字串) 字串轉數字
+        System.out.println(Integer.parseInt(a) + Integer.parseInt(b));
+        
+        
+    }
 }
