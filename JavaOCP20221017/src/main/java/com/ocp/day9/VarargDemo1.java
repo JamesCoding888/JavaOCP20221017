5f1dbdf44d6a67934517b47625cd72f4e36b2b9b
James <JamesCoding888@hotmail.com>
2022-11-22 23:46:16 +0800

    day8

--- JavaOCP20221017/src/main/java/com/ocp/day9/VarargDemo1.java
+++ JavaOCP20221017/src/main/java/com/ocp/day9/VarargDemo1.java
@@ -1,6 +1,22 @@
 package com.ocp.day9;
+/*
+ 	Before Java 5, whenever we wanted to pass an arbitrary number of arguments, 
+ 	we had to pass all arguments in an array or implement N methods (one for each additional parameter):
+ 	
+ 	1) public String format() { ... }
 
+	2) public String format(String value) { ... }
+
+	3) public String format(String val1, String val2) { ... }
+	
+	==========================================================================================================
+	
+	After Java 5, Varargs help us avoid writing boilerplate code by introducing the new syntax 
+	that can handle an arbitrary number of parameters automatically – using an array under the hood.
+
+ */
 public class VarargDemo1 {
+	
     // ... Vararg 自動變長參數
     public static int add(int... values) {
         int sum = 0;
