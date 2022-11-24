e51be78c9bcaa865acbc68cdaf08d764e562990b
James <JamesCoding888@hotmail.com>
2022-11-24 14:30:42 +0800

    day9

---
+++ JavaOCP20221017/src/main/java/com/ocp/day9_2/polymorphism_1/Cat.java
@@ -0,0 +1,11 @@
+package com.ocp.day9_2.polymorphism_1;
+
+public class Cat {
+    String name = "貓";
+    public void 叫聲() {
+        System.out.println("喵~~");
+    }
+    public String getName() {
+        return name;
+    }
+}
---
+++ JavaOCP20221017/src/main/java/com/ocp/day9_2/polymorphism_1/Cow.java
@@ -0,0 +1,5 @@
+package com.ocp.day9_2.polymorphism_1;
+
+public class Cow {
+
+}
