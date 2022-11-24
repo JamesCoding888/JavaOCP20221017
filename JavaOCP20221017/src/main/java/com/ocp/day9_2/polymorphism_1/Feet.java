2f40343752cc58accb73613eb52cd4fd4c2b4d47
James <JamesCoding888@hotmail.com>
2022-11-24 15:08:16 +0800

    day9

--- JavaOCP20221017/src/main/java/com/ocp/day9_2/polymorphism_1/Feet.java
+++ JavaOCP20221017/src/main/java/com/ocp/day9_2/polymorphism_1/Feet.java
@@ -3,5 +3,9 @@ package com.ocp.day9_2.polymorphism_1;
 public class Feet {
 	String name = "feet";
 	
+	public int getFeet() {		
+		return 4;
+	}
+	
 
 }
--- JavaOCP20221017/src/main/java/com/ocp/day9_2/polymorphism_1/TestPolymorphism.java
+++ JavaOCP20221017/src/main/java/com/ocp/day9_2/polymorphism_1/TestPolymorphism.java
@@ -4,12 +4,22 @@ public class TestPolymorphism {
 	public static void main(String[] args) {
 		Cat cat = new Cat();
 		Cat lion = new Lion();
-		
-		cat.animalSound();
-		lion.animalSound();
+		Feet cow = new Cow();
+		Feet horse = new Horse();
+		System.out.println(((Cow)cow).nameOfAnimal);		
+		((Cow)cow).animalSound();
+		System.out.println("How many feet of Cow have? " + cow.getFeet() + " feet");
+		System.out.println(((Horse)horse).nameOfAnimal);
+		((Horse)horse).animalSound();
+		System.out.println("How many feet of Horse have? " + horse.getFeet() + " feet");
 		System.out.println(cat.nameOfAnimal);
-		System.out.println(lion.nameOfAnimal);
-		System.out.println(cat.getName());
-		System.out.println(lion.getName());
+		System.out.println("How many feet of Cat have? " + cat.getFeet() + " feet");
+		cat.animalSound();
+		System.out.println("Omg! What is a creepy sound from " + lion.nameOfAnimal + "!!!");
+		lion.animalSound();
+		System.out.println("Let's downCast the object of Cat.class to sub-class (i.e., Lion.class)");
+		System.out.println(((Lion)lion).nameOfAnimal);
+		lion.animalSound();
+		System.out.println("OH~~~ Thanks God to help me out");
 	}
 }
