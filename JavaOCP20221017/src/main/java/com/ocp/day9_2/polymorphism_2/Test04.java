3283720d9a202754d53f7491aa9f01ca91ab16f6
James <JamesCoding888@hotmail.com>
2022-11-25 21:16:59 +0800

    day9

--- JavaOCP20221017/src/main/java/com/ocp/day9_2/polymorphism_2/Test04.java
+++ JavaOCP20221017/src/main/java/com/ocp/day9_2/polymorphism_2/Test04.java
@@ -2,8 +2,58 @@ package com.ocp.day9_2.polymorphism_2;
 
 public class Test04 {
 	public static void main(String[] args) {
-		
-		
+		Employee employee = new Employee();
+		Manager manager = new Manager();
+		Supervisor supervisor = new Supervisor();
+		// Collect all objects, based on differently positions, with Employee[]
+		Employee[] employees = { employee, manager, supervisor };
+		// for-each 
+		// Print out the value of each employee from invoking their variable(s) by object
+		for (Employee emp : employees) {
+			
+			// Returns the simple name of the each object and salary from each position
+			System.out.println(emp.getClass().getSimpleName() + ", Salary: " + emp.salary);
+			
+			// Print out the Job Description
+			emp.jobDescription();
+			
+			// To confirm if object of emp is inheritance of Manager
+			if(emp.getClass().getSimpleName().equals("Manager")) {
+				// Since the object of Manager 
+				System.out.println("budget: " + ((Manager) emp).budget);
+			}
+			
+			if (emp instanceof Manager) { 
+				System.out.println("budget: " + ((Manager) emp).budget);
+			}
+			
+			// To confirm if object of emp is inheritance of Supervisor
+			if (emp.getClass().getSimpleName().equals("Supervisor")) {
+				System.out.println("stockoption: " + ((Supervisor) emp).stock_option);
+			}
+			
+			if (emp instanceof Supervisor) {
+				System.out.println("stockoption: " + ((Supervisor) emp).stock_option);
+			}
+			
+		}
 
+		// Manager[] managers = {manager, supervisor};
 	}
 }
+
+
+/*
+	Console:
+				Employee, Salary: 80000
+				Programming
+				Manager, Salary: 80000
+				Project Manager
+				budget: 100000
+				budget: 100000
+				Supervisor, Salary: 80000
+				Business Strategy
+				stockoption: 120000
+				stockoption: 120000
+
+*/
--- JavaOCP20221017/src/main/java/com/ocp/day9_2/polymorphism_2/Test05.java
+++ JavaOCP20221017/src/main/java/com/ocp/day9_2/polymorphism_2/Test05.java
@@ -1,30 +1,41 @@
 package com.ocp.day9_2.polymorphism_2;
 
 public class Test05 {
+
 	public static void main(String[] args) {
-		
-		Employee employee = new Manager();
+		Employee employee = new Employee();
+		Manager manager = new Manager();
+		Supervisor supervisor = new Supervisor();
 		
 		System.out.println("Employee Salary: " + employee.salary);
 		
-		// To confirm if object of "employee" has relationship of inheritance between Manager and Employee 
-		if (employee instanceof Manager) {
-			System.out.println("The object of employee is inherited by Manager");
-			System.out.println("Downcast the object of employee from Employee.class to Manager.class, for catching the value of variable from budget in Manager.class: " + ((Manager) employee).budget);
-		}
+		printBudget(employee);
+		printBudget(manager);
+		printBudget(supervisor);
+	}
+
+	public static void printBudget(Employee employee) {
 		
+		// To confirm if object of "employee" has relationship of inheritance between Manager and Employee
+		if (employee instanceof Manager) {
+			System.out.println("The object of employee is inherited by Manager: "  + employee.getClass());
+			System.out.println("Downcast the object of employee from Employee.class to Manager.class, for catching the value of variable from budget in Manager.class: " + ((Manager) employee).budget);
+		}  
+
 		// To confirm if object of "employee" has relationship of inheritance between Supervisor and Employee
 		if (employee instanceof Supervisor) {
-			System.out.println("The object of employee is inherited by Supervisor");
+			System.out.println("The object of employee is inherited by Supervisor: " + employee.getClass());
 			System.out.println(((Supervisor) employee).stock_option);
-		}
+		}			
 	}
 }
 
-
 /*
-		Console:
-			Employee Salary: 80000
-			The object of employee is inherited by Manager
-			Downcast the object of employee from Employee.class to Manager.class, for catching the value of variable from budget in Manager.class: 100000
+	Console:
+				Employee Salary: 80000
+				The object of employee is inherited by Manager: class com.ocp.day9_2.polymorphism_2.Manager
+				Downcast the object of employee from Employee.class to Manager.class, for catching the value of variable from budget in Manager.class: 100000
+				The object of employee is inherited by Supervisor: class com.ocp.day9_2.polymorphism_2.Supervisor
+				120000
+
 */
