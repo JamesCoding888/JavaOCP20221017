a5d0f7438b9df0586550ced886a33b7f32712d58
James <JamesCoding888@hotmail.com>
2022-11-25 23:55:02 +0800

    day9

--- JavaOCP20221017/src/main/java/com/ocp/day10/exception/TestException.java
+++ JavaOCP20221017/src/main/java/com/ocp/day10/exception/TestException.java
@@ -7,7 +7,7 @@ class HandleNumberException extends Exception {
 	}
 
 	public HandleNumberException(int x) {
-		System.out.printf("[Number - Engineer - 0001] End-user enter %d to denominator, engineer team shall record this event! \n\n", x);
+		System.out.printf("[Number - Engineer - 0001] End-user enter %d to denominator, engineer team shall record the log! \n\n", x);
 	}
 
 	public void reDirect2FrontEnd() {
@@ -21,13 +21,16 @@ public class TestException {
 	static int result = 0;
 	public static void main(String[] args) throws HandleNumberException{
 		HandleNumberException handle = new HandleNumberException();			
-		if(x>0) {
-			result = 100/x;	
+		
+		if(x > 0) {
+			result = 100 / x;	
 		}
-		if(x<=0) {
+		
+		if(x <= 0) {
 			handle.reDirect2FrontEnd();
 			throw new HandleNumberException(x);
 		}
+		
 		System.out.println(result);		
 	}
 }
@@ -35,8 +38,9 @@ public class TestException {
 /*
 	Console:
 			[Number - ServiceTeam - 0001] Appologies, our system don't allow '0' number here. Please re-enter your number. Thank you
-			[Number - Engineer - 0001] End-user enter 0 to denominator, engineer team shall record this event! 
+			[Number - Engineer - 0001] End-user enter 0 to denominator, engineer team shall record the log! 
 			
-			Exception in thread "main" com.ocp.day9_2.polymorphism_2.HandleNumberException
-				at com.ocp.day9_2.polymorphism_2.TestException.main(TestException.java:29)
+			Exception in thread "main" com.ocp.day10.exception.HandleNumberException
+				at com.ocp.day10.exception.TestException.main(TestException.java:31)
+
 */
