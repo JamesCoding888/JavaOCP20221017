df58806eb599cc260e2c75054d05f5ba3a498540
James <JamesCoding888@hotmail.com>
2022-11-26 00:31:18 +0800

    day9

--- JavaOCP20221017/src/main/java/com/ocp/day9_2/polymorphism_2/Test05.java
+++ JavaOCP20221017/src/main/java/com/ocp/day9_2/polymorphism_2/Test05.java
@@ -16,13 +16,13 @@ public class Test05 {
 
 	public static void printBudget(Employee employee) {
 		
-		// To confirm if object of "employee" has relationship of inheritance between Manager and Employee
+		// To confirm if the object is an instance of the specified type (i.e., Manager)
 		if (employee instanceof Manager) {
 			System.out.println("The object of employee is inherited by Manager: "  + employee.getClass());
 			System.out.println("The object of employee is downcasted from Employee.class to Manager.class, for catching the value of variable from budget in Manager.class: " + ((Manager) employee).budget);
 		}  
 
-		// To confirm if object of "employee" has relationship of inheritance between Supervisor and Employee
+		// To confirm if the object is an instance of the specified type (i.e., Supervisor)
 		if (employee instanceof Supervisor) {
 			System.out.println("The object of employee is inherited by Supervisor: " + employee.getClass());
 			System.out.println(((Supervisor) employee).stock_option);
