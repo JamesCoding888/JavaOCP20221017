f645b9c24d70ae301a79edb1a834071f8cb44de1
James <JamesCoding888@hotmail.com>
2022-11-26 11:22:26 +0800

    day9

--- JavaOCP20221017/src/main/java/com/ocp/day9_2/polymorphism_2/Test01.java
+++ JavaOCP20221017/src/main/java/com/ocp/day9_2/polymorphism_2/Test01.java
@@ -7,16 +7,19 @@ public class Test01 {
 		System.out.println("Employee salary: $" + employee.salary);
 		System.out.print("Job Description: ");
 		employee.jobDescription();
+		
 		Manager manager = new Manager();		
 		System.out.println("Manager salary: $" + manager.salary);		
 		System.out.println("Budget of Manager: $" + manager.budget);
 		System.out.print("Job Description: ");
 		manager.jobDescription();
+		
 		Supervisor supervisor = new Supervisor();
 		System.out.println("Supervisor salary: $" + supervisor.salary + " What! Are you sure!!!");
 		System.out.println("No worry ~ " + "We supply" + " Stock Option, $" + supervisor.stock_option +  ", for you");
 		System.out.print("Job Description: ");
-		supervisor.jobDescription();		
+		supervisor.jobDescription();
+		
 		System.out.println(((Manager)((Employee)supervisor)).budget); // Bug here - Exception in thread "main" java.lang.ClassCastException
 	}
 }
