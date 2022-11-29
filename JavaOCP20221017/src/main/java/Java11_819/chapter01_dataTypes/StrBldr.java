a2ca1147a7beff8d05e8acf7dabd21d2c42c2903
James <JamesCoding888@hotmail.com>
2022-11-29 16:38:48 +0800

    Java11_819.chapter01_dataTypes

--- JavaOCP20221017/src/main/java/Java11_819/chapter01_dataTypes/StrBldr.java
+++ JavaOCP20221017/src/main/java/Java11_819/chapter01_dataTypes/StrBldr.java
@@ -10,20 +10,20 @@ public class StrBldr {
 	}
 	
 	public static void main(String[] args) {
-//		第一練習： 了解 StringBuilder 的 append API 之用法
+//		第一練習： 了解 StringBuilder 之 "append" API 用法
 /*    
  
 		sb1 = sb1.append("123");  		
 		System.out.println(sb1); //yo 123
 */
 		
-//		第二練習： 了解 foo 有參數方法回傳 new StringBuilder()  與 sb1 進行 append 時，先執行 sb1 再結合 "ey"
+//		第二練習： 了解 foo 有參數方法回傳 new StringBuilder() 與 sb1 物件進行 append 時，先執行 sb1 再串接 "ey"
 /*		
 		sb1 = sb1.append(new StrBldr2().foo(new StringBuilder())); // oh hi 
 		System.out.println(sb1); //  oh hi yo ey
 */
 
-//		第三練習： 了解 foo 有參數方法回傳 new StringBuilder("ey")  與 sb1 進行 append 時，先執行 sb1 再結合 "ey"		
+//		第三練習： 了解 foo 有參數方法回傳 new StringBuilder("ey")  與 sb1 物件進行 append 時，先執行 sb1 再串接 "ey"		
 /*      
 		sb1 = sb1.append(new StrBldr2().foo(sb1));  //yo  oh hi 
 		System.out.println(sb1); //yo  oh hi yo ey
