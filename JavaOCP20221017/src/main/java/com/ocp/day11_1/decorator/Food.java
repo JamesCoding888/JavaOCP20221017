daabea0d4785125faca621da7a7196306a5fe099
James <JamesCoding888@hotmail.com>
2022-11-29 18:18:42 +0800

    day11

--- JavaOCP20221017/src/main/java/com/ocp/day11_1/decorator/Food.java
+++ JavaOCP20221017/src/main/java/com/ocp/day11_1/decorator/Food.java
@@ -1,7 +1,13 @@
 package com.ocp.day11_1.decorator;
+/*
+  
+  	Regarding visibility of protected, 
+  	please recap the previously lecture on "package com.ocp.day2.bar"
+  
+*/
 
 public abstract class Food {
-    
+
 	protected String name;
     
     protected int price;
