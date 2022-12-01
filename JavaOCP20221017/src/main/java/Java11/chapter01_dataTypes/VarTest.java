20ae29e69739afcb3707ece0b5d692e503b90aff
James <JamesCoding888@hotmail.com>
2022-12-01 23:40:49 +0800

    Java11.chapter01_dataTypes

--- JavaOCP20221017/src/main/java/Java11/chapter01_dataTypes/VarTest.java
+++ JavaOCP20221017/src/main/java/Java11/chapter01_dataTypes/VarTest.java
@@ -1,25 +1,23 @@
 package Java11.chapter01_dataTypes;
 
-public class VarTest {
-
-	/*
-	 	What is result of running the following programming?
-	 	
-	 	A. 5
-	 	B. 10
-	 	C. 15
-	 	D. 20
-	 	E. 25
-	 	F. 30
-	*/
+/*
+	What is result of running the following programming?
 	
+	A. 5
+	B. 10
+	C. 15
+	D. 20
+	E. 25
+	F. 30
+*/
+
+public class VarTest {
 	public static void main(String[] args) {
 		var i = 10;
 		var j = 5;
 		final var k = 10;
 		i+=(j*5 + i) / j - 2;
-		System.out.println(i);
-		
+		System.out.println(i);	
 	}
 }
 
@@ -30,10 +28,9 @@ public class VarTest {
 
 
 
-
-
-
 /*
+ 
  	Answer: C
 	本節說明 "var" type，請參 package: Java11.chapter01_dataTypes 的 VarDemo.java 說明
+	
 */
