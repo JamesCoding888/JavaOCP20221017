330a4a496a85903067e474f273dd2b5ca5931a6d
liaojames <liaojames@localhost>
2022-12-02 10:29:26 +0800

    day11

--- JavaOCP20221017/src/main/java/com/ocp/day11_1/decorator/Food.java
+++ JavaOCP20221017/src/main/java/com/ocp/day11_1/decorator/Food.java
@@ -2,7 +2,7 @@ package com.ocp.day11_1.decorator;
 /*
   
   	Regarding visibility of protected, 
-  	please recap the previously lecture on "package com.ocp.day2.bar"
+  	please recap the previously lecture on package: com.ocp.day2.bar
   
 */
 
