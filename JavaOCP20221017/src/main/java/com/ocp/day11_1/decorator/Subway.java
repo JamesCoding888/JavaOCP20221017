bcae9455c3b2dd65105404b7766ba87e14cd84e1
James <JamesCoding888@hotmail.com>
2022-12-04 00:14:53 +0800

    day11

--- JavaOCP20221017/src/main/java/com/ocp/day11_1/decorator/Subway.java
+++ JavaOCP20221017/src/main/java/com/ocp/day11_1/decorator/Subway.java
@@ -1,16 +1,21 @@
 package com.ocp.day11_1.decorator;
 
 /*
- 	Of this lecture - Decorator Pattern, we design a Menu of Subway Order System. 
- 	We design a Scanner object for customer to choose three fixed option of bread. 
+ 	Of this lecture - Decorator Pattern (a menu of Subway Order System). 
+ 	We design a Scanner object for customer to choose three fixed option of bread (i.e., Wheat, Honey Oat, or Italian and Parmesan Oregano). 
  	
- 	(BTW, if you forgot how operation of Scanner and nextLine could be, please refer to package: com.ocp.day6.ScannerPractice)
- 	 
- 	Also, we follow up with the rule of inheritance between each of subclass and superclass. 
- 	Then, you can read the line at 34 and line at 42 to 51. That many different objects from each of classes.
- 	As you see, the objects from o1 to o11. While the each object being initiated, then will implement the variable of "name" from abstraction Food.class. 
- 	Especially, the method of "public String getName(){ ... }" from Flavors.class at line: 14 to 18, which has a detail operation for your information.  
- 
+ 	Well, let's go back to the topic regarding Decorator Pattern. 
+ 	Firstly, you will see the many different initiated objects from o1 to o11. 
+ 	Please read the line at 40 and 48-57. Each of initiated object will be the argument of the next Constructor. 
+ 	It's likely all of them are wrapped up as decoration of furniture at house. 
+ 	For an instance of your house, you may need a big door, chairs, a oval table, a set of sofa, two Televisions, and many bulbs...etc. 
+ 	Those of furniture of house, it seems that you can add or remove whenever you want. 
+ 	For this said, you are hangry now, and need to order a meal of subway. 
+ 	You could choose what kind of bread you need, and also what flavors you would like to decorate into the bread. 
+ 	Just in case of the reason you like to remove one of flavors (e.g., egg), then make an annotation of it. 
+ 	
+ 	By the way, if you forgot how operation of Scanner and nextLine() could be, please refer to the previously package at com.ocp.day6.ScannerPractice
+ 	
 */
 
 import java.util.Scanner;
@@ -72,7 +77,7 @@ public class Subway {
 /*
  	Console: 
 	 		Currently, we only have following options of bread: 1) Wheat 2) Honey Oat 3) Italian and Parmesan Oregano
-			Which type of bread? W h e a t 
+			Which type of bread? W h e a t
 			Currently, we only have following options of bread: 1) Wheat 2) Honey Oat 3) Italian and Parmesan Oregano
 			Which type of bread? Wheat
 			Meal Combination: Onion + Lettuce + Olive oil + Tuna + Egg + Egg + Tomato + Tomato + Ham + Ham + Wheat
