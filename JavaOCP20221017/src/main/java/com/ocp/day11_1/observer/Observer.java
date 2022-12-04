2a06faca5c93eda529c6f6ac56dead2b8366e740
James <JamesCoding888@hotmail.com>
2022-12-04 20:56:55 +0800

    day11

--- JavaOCP20221017/src/main/java/com/ocp/day11_1/observer/Observer.java
+++ JavaOCP20221017/src/main/java/com/ocp/day11_1/observer/Observer.java
@@ -2,6 +2,6 @@ package com.ocp.day11_1.observer;
 
 public interface Observer {
 	
-	void update(String message); // same as "public abstract void update(String message)"
+	void update(String message); // same as "public abstract void update(String message);"
 	
 }
