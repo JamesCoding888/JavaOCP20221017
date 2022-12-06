cd9f9326c4adc7b5a32e339b3cfd0b5bb4c45b35
James <JamesCoding888@hotmail.com>
2022-12-06 16:49:55 +0800

    day12

--- JavaOCP20221017/src/main/java/com/ocp/day12/anonymous/calculation/Calc.java
+++ JavaOCP20221017/src/main/java/com/ocp/day12/anonymous/calculation/Calc.java
@@ -1,11 +1,16 @@
 package com.ocp.day12.anonymous.calculation;
 /*
+ 	==============================================================================================================================================================================
  	Description of FuntionalInterface: 
 	Given an annotation of @FunctionalInterface, to make sure the interface class, only has ONE abstraction method
 	Java 1.8 version, released an annotation of @FunctionalInterface, to help developer to check up the current interface class if meet the specification of FunctionalInterface.
-	Just in case of 2 abstraction method existance in one interface class, the compiler will present the error message as following - Invalid '@FunctionalInterface' annotation; Calc is not a functional interface
+	Just in case of 2 abstraction method existed in one interface class, the compiler will present the error message as follows:
+	"Invalid '@FunctionalInterface' annotation; Calc is not a functional interface"
+	==============================================================================================================================================================================
 	Java 1.8 版本，提供了此註解 @FunctionalInterface，可協助開發者檢查其中的程式碼，是否滿足一個介面，只有一個抽象方法
-	如果有 2 個以上之抽象方法，則 compiler 時，會出現 - Invalid '@FunctionalInterface' annotation; Calc is not a functional interface
+	如果有 2 個以上之抽象方法，則 compiler 時，會出現錯誤訊息，如下: 
+	"Invalid '@FunctionalInterface' annotation; Calc is not a functional interface"
+	==============================================================================================================================================================================
 */
 @FunctionalInterface
 public interface Calc {
---
+++ JavaOCP20221017/src/main/java/com/ocp/day13/StreamDemo.java
@@ -0,0 +1,17 @@
+package com.ocp.day13;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+public class StreamDemo {
+	public static void main(String[] args) {
+		
+		String[] frameworks = {"Spring", "SpringMVC", "SpringBoot", "SpringCloud", "SpringSecurity"};
+		
+		IntStream intStream = Arrays.stream(frameworks)
+									.mapToInt(framework -> framework.length())
+									.peek(System.out::println)        // a peek to print out the length of each element
+									.filter((int length) -> length >= 10);  // filter out the length of element >= 10
+		
+		System.out.println("Length of \"elements >= 10\": " + intStream.count());
+	}
+}
