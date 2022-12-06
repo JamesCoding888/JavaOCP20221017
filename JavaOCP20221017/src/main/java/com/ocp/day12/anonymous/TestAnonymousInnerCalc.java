229880ca931106ea40eae7a5ea65a627cec807ce
James <JamesCoding888@hotmail.com>
2022-12-06 10:52:58 +0800

    day12

--- JavaOCP20221017/src/main/java/com/ocp/day12/anonymous/TestAnonymousInnerCalc.java
+++ JavaOCP20221017/src/main/java/com/ocp/day12/anonymous/TestAnonymousInnerCalc.java
@@ -6,10 +6,10 @@ public class TestAnonymousInnerCalc {
 		
 		int sum = new CalcImplement().submit(5, 5);
 		
-		System.out.println(sum);
+		System.out.println("[1]: " + sum);
 		
 		Calc calc = new CalcImplement();
-		System.out.println(calc.submit(10, 30));
+		System.out.println("[2]: " + calc.submit(10, 30));
 		
 		
 		/*
@@ -36,19 +36,19 @@ public class TestAnonymousInnerCalc {
 
 		int sumByAnonymous = calcByInnerClass.submit(8, 7);
 		
-		System.out.println(sumByAnonymous);
+		System.out.println("[3]: " + sumByAnonymous);
 		
 	    // Lambda expression - Standard Syntax
         Calc calcByLambdaExpression01 = (int x, int y) -> { return x + y; };
-        System.out.println(calcByLambdaExpression01.submit(7, 9));
+        System.out.println("[4]: " + calcByLambdaExpression01.submit(7, 9));
 
         // Lambda expression - allow to remove type (i.e., int) of arguments (Reminder, remove ONLY one type of arguments - Unallowable)        
         Calc calcByLambdaExpression02 = (x, y) -> { return x + y; };
-        System.out.println(calcByLambdaExpression02.submit(1, 3));
+        System.out.println("[5]: " + calcByLambdaExpression02.submit(1, 3));
 
         // Lambda expression - allow to remove type (i.e., int) of arguments and "{ return  ;}"         
         Calc calcByLambdaExpression03 = (x, y) -> x + y ;
-        System.out.println(calcByLambdaExpression03.submit(3, 6));
+        System.out.println("[6]: " + calcByLambdaExpression03.submit(3, 6));
 		
 	}
 }
