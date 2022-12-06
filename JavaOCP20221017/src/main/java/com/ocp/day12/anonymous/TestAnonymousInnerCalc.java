829cd7568f507ba3ed26d06b7e54588747846eb9
James <JamesCoding888@hotmail.com>
2022-12-06 10:01:59 +0800

    day12

--- JavaOCP20221017/src/main/java/com/ocp/day12/anonymous/TestAnonymousInnerCalc.java
+++ JavaOCP20221017/src/main/java/com/ocp/day12/anonymous/TestAnonymousInnerCalc.java
@@ -1,6 +1,6 @@
 package com.ocp.day12.anonymous;
 
-public class TestCalc {
+public class TestAnonymousInnerCalc {
 
 	public static void main(String[] args) {
 		
@@ -12,31 +12,44 @@ public class TestCalc {
 		System.out.println(calc.submit(10, 30));
 		
 		
-		// Anonymous Inner Class
+		/*
+	 		An inner class declared without a Class name is 
+			defined as an Anonymous Inner Class. 
+			In case of anonymous inner classes, we "declare" and 
+			"instantiate" them at the same time. 
+		
+			Generally, they are used whenever you need to override the method of 
+			"a class" or "an interface". 
+		
+			The syntax of an anonymous inner class is as follows:			
+		*/
 		Calc calcByInnerClass = new CalcImplement() {
 			
+			// Override the abstraction method at Calc.class
+			@Override 
 			public int submit(int x, int y) {
 				
-				return x + y;
+				return x + y;	
 				
-			}	
+			}			
 		};
-		
+
 		int sumByAnonymous = calcByInnerClass.submit(8, 7);
 		
 		System.out.println(sumByAnonymous);
 		
 		
-	    // Lambda expression
+	    // Lambda expression - Standard Syntax
         Calc calcByLambdaExpression01 = (int x, int y) -> { return x + y; };
         System.out.println(calcByLambdaExpression01.submit(7, 9));
-        
+
+        // Lambda expression - allow to remove type of arguments (Reminder, remove ONLY one type of arguments - Unallowable)        
         Calc calcByLambdaExpression02 = (x, y) -> { return x + y; };
         System.out.println(calcByLambdaExpression02.submit(1, 3));
-        
+
+        // Lambda expression - allow to remove type of arguments and "{ return  ;}"         
         Calc calcByLambdaExpression03 = (x, y) -> x + y ;
         System.out.println(calcByLambdaExpression03.submit(3, 6));
 		
 	}
-
 }
