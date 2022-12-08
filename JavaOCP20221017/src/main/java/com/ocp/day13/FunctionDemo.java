7819005331eaf9fe5cbb9b10cf34bcf93e5fce21
James <JamesCoding888@hotmail.com>
2022-12-08 16:35:28 +0800

    day13

--- JavaOCP20221017/src/main/java/com/ocp/day13/FunctionDemo.java
+++ JavaOCP20221017/src/main/java/com/ocp/day13/FunctionDemo.java
@@ -1,5 +1,6 @@
 package com.ocp.day13;
 
+import java.util.function.BiFunction;
 import java.util.function.Function;
 public class FunctionDemo {
 
@@ -19,10 +20,49 @@ public class FunctionDemo {
 		Function<String, Integer> functionInShorthandSyntax = string -> string.split(" ").length;
 		Integer lengthFromStandardSyntax = functionInStandardSyntax.apply("Happy Ending 2022");
 		Integer lengthFromShorthandSyntax = functionInShorthandSyntax.apply("Happy Ending 2022");
-		System.out.println(lengthFromStandardSyntax);
-		System.out.println(lengthFromShorthandSyntax);
+		System.out.println("lengthFromStandardSyntax: " + lengthFromStandardSyntax);
+		System.out.println("lengthFromShorthandSyntax: " + lengthFromShorthandSyntax);
 		
 		
+		
+		/*
+		    // Returns a composed function that first applies this function to
+		    // its input, and then applies the {@code after} function to the result.
+		    // If evaluation of either function throws an exception, it is relayed to
+		    // the caller of the composed function.
+		    
+		    // @param <V> the type of output of the {@code after} function, and of the
+		    //           composed function
+		    // @param after the function to apply after this function is applied
+		    // @return a composed function that first applies this function and then
+		    // applies the {@code after} function
+		    // @throws NullPointerException if after is null
+		    
+		    default <V> BiFunction<T, U, V> andThen(Function<? super R, ? extends V> after) {
+		        Objects.requireNonNull(after);
+		        return (T t, U u) -> after.apply(apply(t, u));
+		    }
+	    */
+		BiFunction<String, Integer, Double> biFunction = (String name, Integer length) -> {
+			
+			double area = 0.0;
+			
+			switch(name) {				
+				case "Square":
+					area = Math.pow(length, 2);
+					break;
+				
+				case "Circular":
+					// Circular area formula
+					area = (Math.PI) * (Math.pow(length, 2));
+					break;
+			}
+			
+			return area;
+		};
+		
+		System.out.println("Area of Square: " + biFunction.apply("Square", 10));
+		System.out.println("Circular area: " + biFunction.apply("Circular", 10));
 		
 	}
 }
