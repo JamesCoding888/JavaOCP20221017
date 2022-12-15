62fdcdd6fba9ffad3be204b6d51c8c12c18f3437
James <JamesCoding888@hotmail.com>
2022-12-15 17:31:49 +0800

    day14

--- JavaOCP20221017/src/main/java/com/ocp/day14/hashSet/HashSetDemo2.java
+++ JavaOCP20221017/src/main/java/com/ocp/day14/hashSet/HashSetDemo2.java
@@ -1,4 +1,5 @@
 package com.ocp.day14.hashSet;
+
 import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.Set;
@@ -16,27 +17,48 @@ public class HashSetDemo2 {
         Iterator<Number> iterator1 = scores.iterator();
         while(iterator1.hasNext()) {
         	if(iterator1.next() == null) {
-        		iterator1.remove();  // remote the element(s) of null
+        		iterator1.remove();  // remove the element(s) of null
         	}
         }
         System.out.println(scores);
         
         // Calculate total score
-        int sumOfiterator2= 0;            
+        /* 
+		   The element of Iterator can only be read from the beginning to the end, 			 
+		   and also when we have already read the element, 
+		   we cannot read the element repeatedly.
+		   
+		   Iterator 物件中的元素只能從頭讀到尾，且當我們已讀取過的元素，就不可將元素再讀取一次
+		*/
+        int sum1 = 0;            
         while (iterator1.hasNext()) {
-        	sumOfiterator2 += iterator1.next().intValue();
+        	sum1 += iterator1.next().intValue();
         }
-        System.out.println("Sum: " + sumOfiterator2);
-        int sumOfiterator3 = 0;
+        System.out.println("SumOfIterator2: " + sum1);
+        
+        int sum2 = 0;
         Iterator<Number> iterator3 = scores.iterator();
         while (iterator3.hasNext()) {
-        	sumOfiterator3 += iterator3.next().intValue();
+        	sum2 += iterator3.next().intValue();
         }
         
-        System.out.println("Sum: " + sumOfiterator3);
+        System.out.println("SumOfIterator3: " + sum2);
         
-        // int sum2 = scores.stream().mapToInt(next -> next.intValue()).sum();
-        int sum2 = scores.stream().mapToInt(Number::intValue).sum();
-        System.out.println(sum2);
+        // Java 1.8 - Stream
+        int sumInStandardSyntax = scores.stream().mapToInt((Number next) -> next.intValue()).sum();
+        int sumInShorthandSyntax = scores.stream().mapToInt(Number::intValue).sum();
+        System.out.println("SumOfStream: " + sumInStandardSyntax);
+        System.out.println("SumOfStream: " + sumInShorthandSyntax);
 	}
 }
+
+/*
+	Console:
+			[100, 90, null, 80]
+			[100, 90, 80]
+			SumOfIterator2: 0
+			SumOfIterator3: 270
+			SumOfStream: 270
+			SumOfStream: 270
+
+*/
