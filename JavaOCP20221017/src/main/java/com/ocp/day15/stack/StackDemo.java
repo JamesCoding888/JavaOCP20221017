bafb7113895e13d4f3fe0fc766407f6350d731c5
James <JamesCoding888@hotmail.com>
2022-12-19 15:23:32 +0800

    day15

---
+++ JavaOCP20221017/src/main/java/com/ocp/day15/stack/StackDemo.java
@@ -0,0 +1,30 @@
+package com.ocp.day15.stack;
+
+import java.util.Stack;
+public class StackDemo {
+	public static void main(String[] args) {
+		Stack<String> names = new Stack<>();
+		names.push("1");
+		names.push("2");
+		names.push("3");
+		System.out.println(names);
+		while(!names.isEmpty()) {
+			String name = names.pop();
+			// First in, Last out (先進，後出)
+			System.out.println("Last Out: " + name);
+			System.out.println("Elements: " + names);
+		}
+	}
+}
+
+/*
+	Console:
+			[1, 2, 3]
+			Last Out: 3
+			Elements: [1, 2]
+			Last Out: 2
+			Elements: [1]
+			Last Out: 1
+			Elements: []
+
+*/
