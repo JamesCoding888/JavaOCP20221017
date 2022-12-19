e4a42aeb2d2e6be3be721d47d6f5beeda6b9fe39
James <JamesCoding888@hotmail.com>
2022-12-19 15:59:20 +0800

    day15

--- JavaOCP20221017/src/main/java/com/ocp/day15/stack/StackDemo.java
+++ JavaOCP20221017/src/main/java/com/ocp/day15/stack/StackDemo.java
@@ -4,12 +4,11 @@ package com.ocp.day15.stack;
 	
 	1. public class Stack<E> extends Vector<E>	
 	2. The Stack class represents a last-in-first-out (LIFO) stack of objects. 
-	3. It extends class Vector with five operations that allow a vector to be treated as a stack. 
+	3. It extends class Vector with five operations - empty(), peek(), pop(), push(E item), search(Object o), that allow a vector to be treated as a stack. 
 	4. The usual push and pop operations are provided, as well as a method to peek at the top item on the stack, 
-	   a method to test for whether the stack is empty, and a method to search the stack for an item and discover how far it is from the top.
-	
-	   When a stack is first created, it contains no items.
-	5. Since JDK 1.0
+	   a method to test for whether the stack is empty, and a method to search the stack for an item and discover how far it is from the top.	
+	5. When a stack is first created, it contains no items.
+	6. Since JDK 1.0
 	
 	Refer to the link of Oracle: 
 	https://docs.oracle.com/javase/8/docs/api/java/util/Stack.html
@@ -23,7 +22,16 @@ public class StackDemo {
 		elements.push("2");
 		elements.push("3");
 		System.out.println(elements);
-		while(!elements.isEmpty()) {
+		// The java.util.Stack.search(Object element) method in Java
+		// is used to search for an element in the stack and get its distance from the top				
+		System.out.println("Search for an element in the stack and get its distance from the top: " + elements.search("3"));
+		System.out.println("Search for an element in the stack and get its distance from the top: " + elements.search("2"));
+		System.out.println("Search for an element in the stack and get its distance from the top: " + elements.search("1"));
+		System.out.println("Search for an element in the stack and get its distance from the top: " + elements.search("0"));
+		
+		while(!elements.isEmpty()) {			
+			String peekAtTopOfStack = elements.peek();
+			System.out.println("Peek at the top of elements from Stack: " + peekAtTopOfStack);
 			String string = elements.pop();
 			// last-in-first-out (LIFO) (後進，先出)
 			System.out.println("Last Out: " + string);
@@ -35,11 +43,19 @@ public class StackDemo {
 /*
 	Console:
 			[1, 2, 3]
+			Search for an element in the stack and get its distance from the top: 1
+			Search for an element in the stack and get its distance from the top: 2
+			Search for an element in the stack and get its distance from the top: 3
+			Search for an element in the stack and get its distance from the top: -1
+			Peek at the top of elements from Stack: 3
 			Last Out: 3
 			Elements: [1, 2]
+			Peek at the top of elements from Stack: 2
 			Last Out: 2
 			Elements: [1]
+			Peek at the top of elements from Stack: 1
 			Last Out: 1
 			Elements: []
+
 
 */
