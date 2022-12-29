fd8dcce6c0d67bb0f2ad1ac96922ae2b93c997f0
James <JamesCoding888@hotmail.com>
2022-12-29 11:20:07 +0800

    day15

--- JavaOCP20221017/src/main/java/com/ocp/day15/comparator/Comparator4Books.java
+++ JavaOCP20221017/src/main/java/com/ocp/day15/comparator/Comparator4Books.java
@@ -45,9 +45,57 @@ class Book implements Comparator<Book>{
 		
 	}
 	
+	/*
+	 	 
+	     // Compares its two arguments for order.  Returns a negative integer,
+	     // zero, or a positive integer as the first argument is less than, equal
+	     // to, or greater than the second.<p>
+	     
+	     // The implementor must ensure that {@code sgn(compare(x, y)) ==
+	     // -sgn(compare(y, x))} for all {@code x} and {@code y}.  (This
+	     // implies that {@code compare(x, y)} must throw an exception if and only
+	     // if {@code compare(y, x)} throws an exception.)<p>
+	     //
+	     // The implementor must also ensure that the relation is transitive:
+	     // {@code ((compare(x, y)>0) && (compare(y, z)>0))} implies
+	     // {@code compare(x, z)>0}.<p>
+	     
+	     // Finally, the implementor must ensure that {@code compare(x, y)==0}
+	     // implies that {@code sgn(compare(x, z))==sgn(compare(y, z))} for all
+	     // {@code z}.<p>
+	     
+	     // It is generally the case, but <i>not</i> strictly required that
+	     // {@code (compare(x, y)==0) == (x.equals(y))}.  Generally speaking,
+	     // any comparator that violates this condition should clearly indicate
+	     // this fact.  The recommended language is "Note: this comparator
+	     // imposes orderings that are inconsistent with equals."<p>
+	     
+	     // In the foregoing description, the notation
+	     // {@code sgn(}<i>expression</i>{@code )} designates the mathematical
+	     // <i>signum</i> function, which is defined to return one of {@code -1},
+	     // {@code 0}, or {@code 1} according to whether the value of
+	     // <i>expression</i> is negative, zero, or positive, respectively.
+	     
+	     // @param o1 the first object to be compared.
+	     // @param o2 the second object to be compared.
+	     // @return a negative integer, zero, or a positive integer as the
+	     //         first argument is less than, equal to, or greater than the
+	     //         second.
+	     
+	     //	***************************** Notice Here ***************************** 
+	     // @throws NullPointerException if an argument is null and this
+	     //         comparator does not permit null arguments
+	     // @throws ClassCastException if the arguments' types prevent them from
+	     //         being compared by this comparator.
+	     //	***********************************************************************
+	      
+	     //	int compare(T o1, T o2);
+	      
+	*/
 	@Override
     public int compare(Book b1, Book b2) {
-        return b1.name.compareTo(b2.name);
+		// ASCENDING order 
+        return b1.name.compareTo(b2.name);   
     }
 	
 	public String toString() {
