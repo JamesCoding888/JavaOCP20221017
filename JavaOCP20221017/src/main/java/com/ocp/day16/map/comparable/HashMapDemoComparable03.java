a9bb96b4e74c57788039fdd5fbf8e20309acdbb3
James <JamesCoding888@hotmail.com>
2023-01-04 16:46:15 +0800

    day16

--- JavaOCP20221017/src/main/java/com/ocp/day16/map/comparable/HashMapDemoComparable03.java
+++ JavaOCP20221017/src/main/java/com/ocp/day16/map/comparable/HashMapDemoComparable03.java
@@ -1,27 +1,83 @@
 package com.ocp.day16.map.comparable;
 
-/* 
-	Introduction of this lecture:
-		
-		1) To recap the previously lecture at package: com.ocp.day15.comparable, 
-		   you will learn how to use "Set<Map.Entry<K, V>> entrySet()" to obtain object(s) from Map
-		
-		2) To review the Java 1.8 - Stream API of "mapToInt", at day3, day4 and day5
-		
-		3) There are different optimized syntaxes at (2) and (3) as following annotations
+
+/*
+ 	Introduction of this lecture:
 	
+		1) API of "public static <T, K, U> Collector<T, ?, Map<K,U>> toMap(Function<? super T, ? extends K> keyMapper, Function<? super T, ? extends U> valueMapper)"
+			
+			public final class Collectors {
+			    
+			     * Returns a {@code Collector} that accumulates elements into a
+			     * {@code Map} whose keys and values are the result of applying the provided
+			     * mapping functions to the input elements.
+			     *
+			     * <p>If the mapped keys contain duplicates (according to
+			     * {@link Object#equals(Object)}), an {@code IllegalStateException} is
+			     * thrown when the collection operation is performed.  If the mapped keys
+			     * might have duplicates, use {@link #toMap(Function, Function, BinaryOperator)}
+			     * instead.
+			     *
+			     * <p>There are no guarantees on the type, mutability, serializability,
+			     * or thread-safety of the {@code Map} returned.
+			     *
+			     * @apiNote
+			     * It is common for either the key or the value to be the input elements.
+			     * In this case, the utility method
+			     * {@link java.util.function.Function#identity()} may be helpful.
+			     * For example, the following produces a {@code Map} mapping
+			     * students to their grade point average:
+			     * <pre>{@code
+			     * Map<Student, Double> studentToGPA
+			     *   = students.stream().collect(
+			     *     toMap(Function.identity(),
+			     *           student -> computeGPA(student)));
+			     * }</pre>
+			     * And the following produces a {@code Map} mapping a unique identifier to
+			     * students:
+			     * <pre>{@code
+			     * Map<String, Student> studentIdToStudent
+			     *   = students.stream().collect(
+			     *     toMap(Student::getId,
+			     *           Function.identity()));
+			     * }</pre>
+			     *
+			     * @implNote
+			     * The returned {@code Collector} is not concurrent.  For parallel stream
+			     * pipelines, the {@code combiner} function operates by merging the keys
+			     * from one map into another, which can be an expensive operation.  If it is
+			     * not required that results are inserted into the {@code Map} in encounter
+			     * order, using {@link #toConcurrentMap(Function, Function)}
+			     * may offer better parallel performance.
+			     *
+			     * @param <T> the type of the input elements
+			     * @param <K> the output type of the key mapping function
+			     * @param <U> the output type of the value mapping function
+			     * @param keyMapper a mapping function to produce keys
+			     * @param valueMapper a mapping function to produce values
+			     * @return a {@code Collector} which collects elements into a {@code Map}
+			     * whose keys and values are the result of applying mapping functions to
+			     * the input elements
+			     *
+			     * @see #toMap(Function, Function, BinaryOperator)
+			     * @see #toMap(Function, Function, BinaryOperator, Supplier)
+			     * @see #toConcurrentMap(Function, Function)
+			
+				   public static <T, K, U>
+				   Collector<T, ?, Map<K,U>> toMap(Function<? super T, ? extends K> keyMapper,
+				                                   Function<? super T, ? extends U> valueMapper) {
+				      
+				       return new CollectorImpl<>(HashMap::new,
+				                                  uniqKeysMapAccumulator(keyMapper, valueMapper),
+				                                  uniqKeysMapMerger(),
+				                                  CH_ID);
+				   }
+			}	
 */
-
-
 import java.util.HashMap;
-import java.util.List;
-import java.util.ArrayList;
 import java.util.Map;
-import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.Map.Entry;
-
-
 public class HashMapDemoComparable03 {
 	public static void main(String[] args) {
 		// Instantiate objects of Exam
@@ -45,56 +101,20 @@ public class HashMapDemoComparable03 {
 		
 		System.out.println(hashMap);
 		
-		System.out.println("******************** Practice 1 ********************");
-		// Collect the objects of Exam which "score >= 60" into object of arrayListForQualifiedExam01
-		List<Exam> arrayListForQualifiedExam01 = new ArrayList<>();
-		for(Entry<Student, Exam> entry : hashMap.entrySet()) {
-			System.out.printf("(1) [Key : Value] = [%s : %s] \n", entry.getKey(), entry.getValue());
-			if(entry.getValue().getScore() >= 60) {
-				System.out.printf("(2) [Key : Value] = [%s : %s] \n", entry.getKey(), entry.getValue());
-				arrayListForQualifiedExam01.add(entry.getValue());
-			}
-		}
-		System.out.println("arrayListForQualifiedExam01: " + arrayListForQualifiedExam01);
-	
-		System.out.println("******************** Practice 2 ********************");
-		// Collect the objects of Exam which "score >= 60" into object of arrayListForQualifiedStudent		
-		Set<Entry<Student, Exam>> set01 = hashMap.entrySet();
-		List<Student> arrayListForQualifiedStudent = 		
-				set01.stream()
-				     .filter((Map.Entry<Student, Exam> entry) -> entry.getValue().getScore() >= 60)
-				     .map(Entry::getKey)
-				     .collect(Collectors.toList());
-		System.out.println("arrayListForQualifiedStudent: " + arrayListForQualifiedStudent);
-		
-		System.out.println("******************** Practice 3 ********************");		
-		// Collect the objects of Exam which "score >= 60" into object of arrayListForQualifiedExam02
-	    List<Exam> arrayListForQualifiedExam02 = 
-	    		hashMap.entrySet()
-			           .stream()
-				       .filter(entry -> entry.getValue().getScore() >= 60)		 
-				       .map(Entry::getValue)
-					   .collect(Collectors.toList());
-	    System.out.println("arrayListForQualifiedExam02: " + arrayListForQualifiedExam02);
+		Map<String, Integer> map = hashMap.entrySet()
+										  .stream()
+										  .collect(Collectors.toMap(
+														 (Entry<Student, Exam> entry) -> entry.getKey().getName(),
+											             (Entry<Student, Exam> entry) -> entry.getValue().getScore()
+												  ));
+		System.out.println(map);
 	}
 }
 
 /*
 	Console:
 			{Student [id=3, name=David]=Exam [subject=RFIC Design, score=59], Student [id=2, name=James]=Exam [subject=Microwave Engineering, score=88], Student [id=1, name=Betty]=Exam [subject=Mathematics, score=98], Student [id=4, name=Elon]=Exam [subject=Java-1z0-819, score=87]}
-			******************** Practice 1 ********************
-			(1) [Key : Value] = [Student [id=3, name=David] : Exam [subject=RFIC Design, score=59]] 
-			(1) [Key : Value] = [Student [id=2, name=James] : Exam [subject=Microwave Engineering, score=88]] 
-			(2) [Key : Value] = [Student [id=2, name=James] : Exam [subject=Microwave Engineering, score=88]] 
-			(1) [Key : Value] = [Student [id=1, name=Betty] : Exam [subject=Mathematics, score=98]] 
-			(2) [Key : Value] = [Student [id=1, name=Betty] : Exam [subject=Mathematics, score=98]] 
-			(1) [Key : Value] = [Student [id=4, name=Elon] : Exam [subject=Java-1z0-819, score=87]] 
-			(2) [Key : Value] = [Student [id=4, name=Elon] : Exam [subject=Java-1z0-819, score=87]] 
-			arrayListForQualifiedExam01: [Exam [subject=Microwave Engineering, score=88], Exam [subject=Mathematics, score=98], Exam [subject=Java-1z0-819, score=87]]
-			******************** Practice 2 ********************
-			arrayListForQualifiedStudent: [Student [id=2, name=James], Student [id=1, name=Betty], Student [id=4, name=Elon]]
-			******************** Practice 3 ********************
-			arrayListForQualifiedExam02: [Exam [subject=Microwave Engineering, score=88], Exam [subject=Mathematics, score=98], Exam [subject=Java-1z0-819, score=87]]
+			{James=88, Betty=98, Elon=87, David=59}
 
 
 */
