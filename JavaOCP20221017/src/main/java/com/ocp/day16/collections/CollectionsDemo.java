87f84a820187b296cee412ec091a6307c36a0b22
James <JamesCoding888@hotmail.com>
2023-01-06 17:28:51 +0800

    day16

--- JavaOCP20221017/src/main/java/com/ocp/day16/collections/CollectionsDemo.java
+++ JavaOCP20221017/src/main/java/com/ocp/day16/collections/CollectionsDemo.java
@@ -8,23 +8,40 @@ import java.util.List;
 public class CollectionsDemo {
 	public static void main(String[] args) {
 		
-		List<Integer> arrayList = new ArrayList<>();
-		arrayList.add(7);   //  7 % 3 = 1
-		arrayList.add(3);   //  3 % 3 = 0
-		arrayList.add(9);   //  9 % 3 = 0
-		arrayList.add(15);  // 15 % 3 = 0
-		System.out.println(arrayList);
+		List<Integer> arrayList1 = new ArrayList<>();
+		List<Integer> arrayList2 = new ArrayList<>();
 		
-		// Traditional implementation of Comparator
-		Collections.sort(arrayList, new Comparator<Integer>() {			
+		arrayList1.add(7);   //  7 % 3 = 1
+		arrayList1.add(3);   //  3 % 3 = 0
+		arrayList1.add(9);   //  9 % 3 = 0
+		arrayList1.add(15);  // 15 % 3 = 0
+		
+		arrayList2.add(7);   //  7 % 3 = 1
+		arrayList2.add(3);   //  3 % 3 = 0
+		arrayList2.add(9);   //  9 % 3 = 0
+		arrayList2.add(15);  // 15 % 3 = 0
+		
+		// Anonymous inner class for implementation of Comparator
+		// Please refer to TestAnonymousInnerSpeedLimits.java, at package: com.ocp.day12.anonymous.speedLimits
+		/*
+	 		An inner class declared without a Class name is 
+			defined as an Anonymous Inner Class. 
+			
+			In case of anonymous inner classes, we "declare" and 
+			"instantiate" them at the same time. 
+		
+			Generally speaking, they are used whenever you need to override the method of 
+			"a class" or "an interface". 						
+		*/
+		Collections.sort(arrayList1, new Comparator<Integer>() {			
 			
 			// (1) ASCENDING order
-			/*
+//			/*
 				@Override
 				public int compare(Integer o1 , Integer o2) {				
 					return o1 - o2;				
 				}
-			*/
+//			*/
 				
 			// (2) DESCENDING order			
 			/*
@@ -43,16 +60,39 @@ public class CollectionsDemo {
 			*/				
 			
 			// (4) DESCENDING order by "result of o % 3"		
-//			/*
+			/*
 				@Override
 				public int compare(Integer o1 , Integer o2) {				
 					return (o2 % 3) - (o1 % 3);				
 				}
-//			*/	
+			*/	
 		});
 		// print out the element(s) of arrayList based on the implementation of Comparator 
-		System.out.println(arrayList);
+		System.out.println("Anonymous inner class for implementation of Comparator: " + arrayList1);
 		
+		
+		// Lambda's implementation of Comparator
+		// (1) ASCENDING order	
+//		/*	
+			Comparator<Integer> comparatorInAscendingOrder = (Integer o1, Integer o2) -> (o1 - o2);
+			Collections.sort(arrayList2, comparatorInAscendingOrder);
+//		*/
+		// (2) DESCENDING order
+		/*
+			Comparator<Integer> comparatorInDescendingOrder = (Integer o1, Integer o2) -> (o2 - o1);
+			Collections.sort(arrayList2, comparatorInDescendingOrder);
+		*/
+		// (3) ASCENDING order by "result of o % 3"
+		/*
+			Comparator<Integer> comparatorInAscendingOrderByＭodulo = (o1, o2) -> ((o1 % 3) - (o2 % 3));
+			Collections.sort(arrayList2, comparatorInAscendingOrderByＭodulo);
+		*/
+		// (4) DESCENDING order by "result of o % 3"						
+		/*
+			Comparator<Integer> comparatorInDescendingOrderByＭodulo = (o1, o2) -> ((o2 % 3) - (o1 % 3));
+			Collections.sort(arrayList2, comparatorInDescendingOrderByＭodulo);
+		*/		
+		System.out.println("Lambda's implementation of Comparator: " + arrayList2);
 		
 	}
 }
@@ -60,20 +100,20 @@ public class CollectionsDemo {
 
 /*
 	Console (1): 
-				[7, 3, 9, 15]
-				[3, 7, 9, 15]
+				Anonymous inner class for implementation of Comparator: [3, 7, 9, 15]
+				Lambda's implementation of Comparator: [3, 7, 9, 15]
 		
 	Console (2): 
-				[7, 3, 9, 15]
-				[15, 9, 7, 3]
-				
+				Anonymous inner class for implementation of Comparator: [15, 9, 7, 3]
+				Lambda's implementation of Comparator: [15, 9, 7, 3]
+												
 	Console (3): 				
-				[7, 3, 9, 15]
-				[3, 9, 15, 7]
+				Anonymous inner class for implementation of Comparator: [3, 9, 15, 7]
+				Lambda's implementation of Comparator: [3, 9, 15, 7]							
 				
 	Console (4):
-				[7, 3, 9, 15]
-				[7, 3, 9, 15]
-
-	 
+				Anonymous inner class for implementation of Comparator: [7, 3, 9, 15]
+				Lambda's implementation of Comparator: [7, 3, 9, 15]	
+				
+		
 */
