6fe9ec5380110e95d8cfaf4ac3f460098cc3547f
James <JamesCoding888@hotmail.com>
2023-01-10 09:59:36 +0800

    day17

--- JavaOCP20221017/src/main/java/com/ocp/day17/exception/ExceptionDemo.java
+++ JavaOCP20221017/src/main/java/com/ocp/day17/exception/ExceptionDemo.java
@@ -1,5 +1,31 @@
 package com.ocp.day17.exception;
 
+/*
+	Noticed here !!! Shall NOT Throw RuntimeException, Exception, or Throwable. 
+	
+	E.g.,
+		public static void main(String[] args) throws RuntimeException, Exception, Throwable { ... }
+		
+	
+	
+	Reason of NOT throwing RuntimeException:
+		
+		1) Throwing a RuntimeException can lead to subtle errors; for example, a caller cannot 
+		   examine the exception to determine why it was thrown and consequently cannot attempt recovery.
+	
+	
+	
+	Supposed to do:
+		
+		1) Methods can throw a customized exception or a sub-classes exception from Exception or RuntimeException. 
+	
+	
+	
+	Refer to the link as follows:
+	https://wiki.sei.cmu.edu/confluence/display/java/ERR07-J.+Do+not+throw+RuntimeException%2C+Exception%2C+or+Throwable
+	
+*/
+
 class HandleNumberException extends Exception {
 
 	public HandleNumberException() {
@@ -20,31 +46,6 @@ public class ExceptionDemo {
 	static int x = 0;
 	static int result = 0;
 	
-	/*
-		Noticed here !!! Shall NOT Throw RuntimeException, Exception, or Throwable. 
-		
-		E.g.,
-			public static void main(String[] args) throws RuntimeException, Exception, Throwable{
-			
-		
-		Reason of NOT throwing RuntimeException:
-			
-			1) Throwing a RuntimeException can lead to subtle errors; for example, a caller cannot 
-			   examine the exception to determine why it was thrown and consequently cannot attempt recovery.
-		
-		
-		
-		Supposed to do:
-			
-			1) Methods can throw a customized exception or a sub-classes exception from Exception or RuntimeException. 
-	
-		
-		
-		Refer to the link as follows:
-		https://wiki.sei.cmu.edu/confluence/display/java/ERR07-J.+Do+not+throw+RuntimeException%2C+Exception%2C+or+Throwable
-		
-		
-	*/
 	public static void main(String[] args) throws HandleNumberException{
 		HandleNumberException handle = new HandleNumberException();			
 		
