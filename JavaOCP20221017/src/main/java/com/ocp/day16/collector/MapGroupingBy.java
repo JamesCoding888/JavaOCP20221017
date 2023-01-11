c940bfdee66af2886cff5ad2300bdf85cc20641c
James <JamesCoding888@hotmail.com>
2023-01-11 14:48:14 +0800

    day16

--- JavaOCP20221017/src/main/java/com/ocp/day16/collector/MapGroupingBy.java
+++ JavaOCP20221017/src/main/java/com/ocp/day16/collector/MapGroupingBy.java
@@ -1,5 +1,5 @@
 package com.ocp.day16.collector;
-import java.util.Arrays; 
+import java.util.Arrays;  
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -7,7 +7,6 @@ import java.util.stream.Collectors;
 import static java.util.stream.Collectors.mapping;
 import static java.util.stream.Collectors.toSet;
 public class MapGroupingBy {
-	
 	public static void main(String[] args) {	
 		
 		List<PurchasedOrder> bookList = Arrays.asList(
@@ -26,25 +25,25 @@ public class MapGroupingBy {
 		Map<String, Long> collectedProductName4RepeatedQuantity = bookList.stream().collect(
 				Collectors.groupingBy(PurchasedOrder::getProductName, Collectors.counting())
 		);		
-		System.out.println(collectedProductName4RepeatedQuantity);
+		System.out.println("collectedProductName4RepeatedQuantity: " + collectedProductName4RepeatedQuantity);
 		
 		// Collect the data of productName v.s. quantity of book
 		Map<String, Integer> collectedProductName4QuantityInGroupingBy = bookList.stream().collect(
 				Collectors.groupingBy(PurchasedOrder::getProductName, Collectors.summingInt(PurchasedOrder::getQuantity))
 		);				
-		System.out.println(collectedProductName4QuantityInGroupingBy);
+		System.out.println("collectedProductName4QuantityInGroupingBy: " + collectedProductName4QuantityInGroupingBy);
 
 		// Collect the data of productName v.s. price of book
 		Map<String, Double> collectedProductName4PriceInGroupingBy = bookList.stream().collect(
 				Collectors.groupingBy(PurchasedOrder::getProductName, Collectors.summingDouble(PurchasedOrder::getPrice))
 		);				
-		System.out.println(collectedProductName4PriceInGroupingBy);
+		System.out.println("collectedProductName4PriceInGroupingBy: " + collectedProductName4PriceInGroupingBy);
 
 		// Collect the data of price of book v.s. List<PurchasedOrder>
 		Map<Double, List<PurchasedOrder>> collectedPrice4BookList = bookList.stream().collect(
 				Collectors.groupingBy(PurchasedOrder::getPrice)			
 		);
-		System.out.println(collectedPrice4BookList);
+		System.out.println("collectedPrice4BookList: " + collectedPrice4BookList);
 		
 		// Collect the data of price of book v.s. Set<String>
 		Map<Double, Set<String>> collectedPrice4ProductNameInGroupingBy = bookList.stream().collect(
@@ -59,7 +58,7 @@ public class MapGroupingBy {
 				Collectors.groupingBy(PurchasedOrder::getPrice, mapping(PurchasedOrder::getProductName, toSet()))				
 		);
 		
-		System.out.println(collectedPrice4ProductNameInGroupingBy);				
+		System.out.println("collectedPrice4ProductNameInGroupingBy: " + collectedPrice4ProductNameInGroupingBy);				
 	
 	}
 }
@@ -67,9 +66,10 @@ public class MapGroupingBy {
 /*
 	Console:
 			[PurchasedOrder [productName=English, quantity=3, price=32.0], PurchasedOrder [productName=Math, quantity=5, price=35.0], PurchasedOrder [productName=Math, quantity=5, price=35.0], PurchasedOrder [productName=Math, quantity=5, price=35.0], PurchasedOrder [productName=Science, quantity=2, price=40.0], PurchasedOrder [productName=Math, quantity=5, price=35.0], PurchasedOrder [productName=History, quantity=2, price=40.0]]
-			{English=1, Science=1, History=1, Math=4}
-			{English=3, Science=2, History=2, Math=20}
-			{English=32.0, Science=40.0, History=40.0, Math=140.0}
-			{32.0=[PurchasedOrder [productName=English, quantity=3, price=32.0]], 35.0=[PurchasedOrder [productName=Math, quantity=5, price=35.0], PurchasedOrder [productName=Math, quantity=5, price=35.0], PurchasedOrder [productName=Math, quantity=5, price=35.0], PurchasedOrder [productName=Math, quantity=5, price=35.0]], 40.0=[PurchasedOrder [productName=Science, quantity=2, price=40.0], PurchasedOrder [productName=History, quantity=2, price=40.0]]}
-			{32.0=[English], 35.0=[Math], 40.0=[Science, History]}
+			collectedProductName4RepeatedQuantity: {English=1, Science=1, History=1, Math=4}
+			collectedProductName4QuantityInGroupingBy: {English=3, Science=2, History=2, Math=20}
+			collectedProductName4PriceInGroupingBy: {English=32.0, Science=40.0, History=40.0, Math=140.0}
+			collectedPrice4BookList: {32.0=[PurchasedOrder [productName=English, quantity=3, price=32.0]], 35.0=[PurchasedOrder [productName=Math, quantity=5, price=35.0], PurchasedOrder [productName=Math, quantity=5, price=35.0], PurchasedOrder [productName=Math, quantity=5, price=35.0], PurchasedOrder [productName=Math, quantity=5, price=35.0]], 40.0=[PurchasedOrder [productName=Science, quantity=2, price=40.0], PurchasedOrder [productName=History, quantity=2, price=40.0]]}
+			collectedPrice4ProductNameInGroupingBy: {32.0=[English], 35.0=[Math], 40.0=[Science, History]}
+
 */
