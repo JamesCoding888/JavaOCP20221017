a9a0f0ea3df8d6e08bd7fafcf54c0fc23a54e908
James <JamesCoding888@hotmail.com>
2023-01-15 12:20:05 +0800

    day16

--- JavaOCP20221017/src/main/java/com/ocp/day16/collector/MapGroupingBy.java
+++ JavaOCP20221017/src/main/java/com/ocp/day16/collector/MapGroupingBy.java
@@ -1,52 +1,6 @@
 package com.ocp.day16.collector;
 /*
-	Introduction of Arrays.asList:
-     
-     * Returns a fixed-size list backed by the specified array. Changes made to
-     * the array will be visible in the returned list, and changes made to the
-     * list will be visible in the array. The returned list is
-     * {@link Serializable} and implements {@link RandomAccess}.
-     *
-     * <p>The returned list implements the optional {@code Collection} methods, except
-     * those that would change the size of the returned list. Those methods leave
-     * the list unchanged and throw {@link UnsupportedOperationException}.
-     *
-     * @apiNote
-     * This method acts as bridge between array-based and collection-based
-     * APIs, in combination with {@link Collection#toArray}.
-     *
-     * <p>This method provides a way to wrap an existing array:
-     * <pre>{@code
-     *     Integer[] numbers = ...
-     *     ...
-     *     List<Integer> values = Arrays.asList(numbers);
-     * }</pre>
-     *
-     * <p>This method also provides a convenient way to create a fixed-size
-     * list initialized to contain several elements:
-     * <pre>{@code
-     *     List<String> stooges = Arrays.asList("Larry", "Moe", "Curly");
-     * }</pre>
-     *
-     * <p><em>The list returned by this method is modifiable.</em>
-     * To create an unmodifiable list, use
-     * {@link Collections#unmodifiableList Collections.unmodifiableList}
-     * or <a href="List.html#unmodifiable">Unmodifiable Lists</a>.
-     *
-     * @param <T> the class of the objects in the array
-     * @param a the array by which the list will be backed
-     * @return a list view of the specified array
-     * @throws NullPointerException if the specified array is {@code null}
-	   
-	   @SafeVarargs
-	   @SuppressWarnings("varargs")
-	   public static <T> List<T> asList(T... a) {
-	      return new ArrayList<>(a);
-	   }
-    
-    
-	 
-	
+  	 	
 	Collections.sort
 		-   
 	
