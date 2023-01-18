bcba729833ec7d607481ff97cc312715c3ff9ea8
James <JamesCoding888@hotmail.com>
2023-01-18 11:30:59 +0800

    day17

--- JavaOCP20221017/src/main/java/com/ocp/day17/exception/IndexOutOfRangeExceptionDemo.java
+++ JavaOCP20221017/src/main/java/com/ocp/day17/exception/IndexOutOfRangeExceptionDemo.java
@@ -14,8 +14,8 @@ class IndexOutOfBoundsExceptionHandler extends IndexOutOfBoundsException {
 
 public class IndexOutOfRangeExceptionDemo {
 	public static void main(String[] args) {
-		String[] names = { "james", "tim", "tom", "david", "Kathy", "Ketty", "Marry" }; 
-		System.out.println(names[0]); // james
+		String[] names = { "James", "Tim", "Tom", "David", "Kathy", "Ketty", "Marry" }; 
+		System.out.println(names[0]); // James
 		System.out.println(names[6]); // Marry
 		/*
 			To be noticed here:
@@ -52,18 +52,18 @@ public class IndexOutOfRangeExceptionDemo {
 
 /*
 	Console 1):
-				james
+				James
 				Marry
 				Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: Index 7 out of bounds for length 7
 					at com.ocp.day17.exception.IndexOutOfRangeExceptionDemo.main(IndexOutOfRangeExceptionDemo.java:25)
 	
 	Console 2):
-				james
+				James
 				Marry
-				james
-				tim
-				tom
-				david
+				James
+				Tim
+				Tom
+				David
 				Kathy
 				Ketty
 				Marry
@@ -72,12 +72,12 @@ public class IndexOutOfRangeExceptionDemo {
 				Continue to run the following code without terminating the current thread
 	
 	Console 3):
-				james
+				James
 				Marry
-				james
-				tim
-				tom
-				david
+				James
+				Tim
+				Tom
+				David
 				Kathy
 				Ketty
 				Marry
