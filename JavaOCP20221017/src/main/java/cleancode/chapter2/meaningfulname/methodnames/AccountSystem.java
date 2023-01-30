f4905f5f51fa4c4e847b512882ae204ca4fed159
James <JamesCoding888@hotmail.com>
2023-01-30 14:26:48 +0800

    chapter2

--- JavaOCP20221017/src/main/java/cleancode/chapter2/meaningfulname/methodnames/AccountSystem.java
+++ JavaOCP20221017/src/main/java/cleancode/chapter2/meaningfulname/methodnames/AccountSystem.java
@@ -1,10 +1,11 @@
-package cleancode.chapter2.meaningfulname;
+package cleancode.chapter2.meaningfulname.methodnames;
 
 /*
-	Class names and Method Names:
+ 	To be noticed the naming of following:
+	-	Class Names
+	-	Method Names
+	-	Constructors 		
 		
-		When constructors are overloaded, use static factory methods with names that describe the arguments.
-		refer to the handbook - "Clean Code_ A Handbook of Agile Software Craftsmanship", at page 25
 */
 
 // Classes and objects should have noun or noun phrase names, also shall NOT be a verb
@@ -39,28 +40,45 @@ public final class AccountSystem {
 		this.salary = salary;
 	}
 
+	// Methods should have verb or verb phrase names
+	public String getPersonalName() {
+		return name;
+	}		
+	
 	public static void main(String[] args) {
 		// Creating an object of AccountSystem and
 		// calling an static factory method valueOf()
 		AccountSystem accountSystem4Manager = AccountSystem.valueOf("MA-12890", "James Bob");
 		AccountSystem accountSystem4IT = AccountSystem.valueOf("IT-14440", "Bobby Go", "Senior Software Engineer", "300000");
+		AccountSystem accountSystem4Sales = AccountSystem.valueOf("Sales-180", "David S.", "Senior Market Developing Executor", "1000000");
 		/*
 		 	Generally better than:
 		 		
 		 		AccountSystem accountSystem4Manager = new AccountSystem("MA-12890", "James Bob");
 		 		AccountSystem accountSystem4IT = new AccountSystem("IT-14440", "Bobby Go", "Senior Software Engineer", "300000");
+		 		AccountSystem accountSystem4Sales = new AccountSystem("Sales-180", "David S.", "Senior Market Developing Executor", "1000000");
 		*/
+		
+		// get Personal name
+		System.out.println(accountSystem4Manager.getPersonalName());
+		System.out.println(accountSystem4IT.getPersonalName());
+		System.out.println(accountSystem4Sales.getPersonalName());
 		
 		// Print out
 		System.out.println("{" + accountSystem4Manager.id + ", " + accountSystem4Manager.name +"}");
 		System.out.println("{" + accountSystem4IT.id + ", " + accountSystem4IT.name + ", " + accountSystem4IT.position + ", " + accountSystem4IT.salary + "}");
+		System.out.println("{" + accountSystem4Sales.id + ", " + accountSystem4Sales.name + ", " + accountSystem4Sales.position + ", " + accountSystem4Sales.salary + "}");
+					
 	}
 }
 
 /*
 	Console:
+				James Bob
+				Bobby Go
+				David S.
 				{MA-12890, James Bob}
 				{IT-14440, Bobby Go, Senior Software Engineer, 300000}
-
+				{Sales-180, David S., Senior Market Developing Executor, 1000000}
 
 */
