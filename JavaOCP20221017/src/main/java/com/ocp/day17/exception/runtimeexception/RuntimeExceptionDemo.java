2bd8b563bb5b2a0aa9e39ca28715227c8d8d91bc
James <JamesCoding888@hotmail.com>
2023-02-16 19:20:26 +0800

    day17

--- JavaOCP20221017/src/main/java/com/ocp/day17/exception/runtimeexception/RuntimeExceptionDemo.java
+++ JavaOCP20221017/src/main/java/com/ocp/day17/exception/runtimeexception/RuntimeExceptionDemo.java
@@ -3,14 +3,14 @@ package com.ocp.day17.exception.runtimeexception;
 // Console 2):
 /*
 class ScaleOverLimitedException extends RuntimeException{
-	
+	// Must have default constructor because of inheritance of ScaleOverLimitedException
 	public ScaleOverLimitedException() {
 		super();
 	}
 	
 	public ScaleOverLimitedException(Exception e) {
 		System.out.printf("[Number - Engineer - 0002] End-user enters scores over the limitedness of printScore(Double scores) method\n");
-		System.out.printf("[Number - ServiceTeam - 0002] My appologies, please enter scores between 60.00 to 100\n");
+		System.out.printf("[Number - ServiceTeam - 0002] My apologies, please enter scores between 60.00 to 100\n");
 	}
 }
 */
@@ -28,47 +28,74 @@ public class RuntimeExceptionDemo
 				  	Notice here - Don't throw runtimeException !!! Otherwise, the main thread will be terminated.
 				  	Then, the following code or services will NOT proceed.
 			*/				
-//			throw runtimeException;
+			throw runtimeException;
 			
 			// Console 2):
 //			throw new ScaleOverLimitedException(runtimeException);
 			
 			// Console 3): 
-			System.out.println(runtimeException);
+//			System.out.println(runtimeException);
 		}
 		boolean isPass = score >= 60;
 		System.out.printf("Scores: %.2f -> %s\n", score, isPass? "Pass" : "Fail");
 	}
 	
 	public static void main(String[] args) {
+		// A. Without try-catch statement
 		// Notice here - round off to 2nd decimal place
-		printScore(59.125); 
-		printScore(60.514);
-		// RuntimeException
-		printScore(100.001);
-		printScore(60.0); 
+		/*
+			printScore(59.125); 
+			printScore(60.514);
+			// RuntimeException
+			printScore(100.001);
+			printScore(60.0); 
+		*/
+		
+		// B. With try-catch statement
+		// Notice here - round off to 2nd decimal place
+//		/*
+			try {
+				printScore(59.125); 
+				printScore(60.514);
+				// RuntimeException
+				printScore(100.001);
+				printScore(60.0);	
+			} catch (RuntimeException e) {
+				System.out.printf("Error message: %s\n", e.getMessage());
+			}
+//		*/
 	}	
 }
 
 
 /*
-	Console 1) - w/ "throw runtimeException": 			
-				Scores: 59.13 -> Fail
-				Scores: 60.51 -> Pass
-				Exception in thread "main" java.lang.RuntimeException: scale over limitedness !!!
-						
-	Console 2) - w/ "throw new ScaleOverLimitedException(runtimeException)":	
-				Scores: 59.13 -> Fail
-				Scores: 60.51 -> Pass
-				[Number - Engineer - 0002] End-user enters scores over the limitedness of printScore(Double scores) method
-				[Number - ServiceTeam - 0002] My appologies, please enter scores between 60.00 to 100
-				Exception in thread "main" com.ocp.day17.exception.runtimeexception.ScaleOverLimitedException
+	A. Without try-catch statement
 	
-	Console 3) - w/o "throw runtimeException" & "throw new ScaleOverLimitedException(runtimeException)":
-				Scores: 59.13 -> Fail
-				Scores: 60.51 -> Pass
-				java.lang.RuntimeException: scale over limitedness !!!
-				Scores: 100.00 -> Pass
-				Scores: 60.00 -> Pass
+		Console 1) - w/ "throw runtimeException": 			
+					Scores: 59.13 -> Fail
+					Scores: 60.51 -> Pass
+					Exception in thread "main" java.lang.RuntimeException: scale over limitedness !!!
+							
+		Console 2) - w/ "throw new ScaleOverLimitedException(runtimeException)":	
+					Scores: 59.13 -> Fail
+					Scores: 60.51 -> Pass
+					[Number - Engineer - 0002] End-user enters scores over the limitedness of printScore(Double scores) method
+					[Number - ServiceTeam - 0002] My appologies, please enter scores between 60.00 to 100
+					Exception in thread "main" com.ocp.day17.exception.runtimeexception.ScaleOverLimitedException
+		
+		Console 3) - w/o "throw runtimeException" & "throw new ScaleOverLimitedException(runtimeException)":
+					Scores: 59.13 -> Fail
+					Scores: 60.51 -> Pass
+					java.lang.RuntimeException: scale over limitedness !!!
+					Scores: 100.00 -> Pass
+					Scores: 60.00 -> Pass
+	
+	B. With try-catch statement
+	
+		Console 1) - w/ "throw runtimeException": 	
+					Scores: 59.13 -> Fail
+					Scores: 60.51 -> Pass
+					Error message: scale over limitedness !!!
 
+	
 */
