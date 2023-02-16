1e7420b20e1ee02c8a4465175fff6b50ff225e49
James <JamesCoding888@hotmail.com>
2023-02-16 16:33:24 +0800

    day17

--- JavaOCP20221017/src/main/java/com/ocp/day17/exception/runtimeexception/RuntimeExceptionDemo.java
+++ JavaOCP20221017/src/main/java/com/ocp/day17/exception/runtimeexception/RuntimeExceptionDemo.java
@@ -1,17 +1,40 @@
 package com.ocp.day17.exception.runtimeexception;
 
-public class RuntimeExceptionDemo {
+// Console 2):
+/*
+class ScaleOverLimitedException extends RuntimeException{
 	
+	public ScaleOverLimitedException() {
+		super();
+	}
+	
+	public ScaleOverLimitedException(Exception e) {
+		System.out.printf("[Number - Engineer - 0002] End-user enters scores over the limitedness of printScore(Double scores) method\n");
+		System.out.printf("[Number - ServiceTeam - 0002] My appologies, please enter scores between 60.00 to 100\n");
+	}
+}
+*/
+
+public class RuntimeExceptionDemo 
+	 // Console 2): 
+//	 extends ScaleOverLimitedException 
+{
+
 	public static void printScore(Double score) {
 		if(score < 0 || score > 100) {
 			RuntimeException runtimeException = new RuntimeException("scale over limitedness !!!");
-			System.out.println(runtimeException);
-			/*
-			  	Notice here - Don't throw runtimeException !!! Otherwise, the main thread will be terminated.
-			  	Then, the following code or services will NOT proceed.
-			*/	
+			/*  
+			 	Console 1):
+				  	Notice here - Don't throw runtimeException !!! Otherwise, the main thread will be terminated.
+				  	Then, the following code or services will NOT proceed.
+			*/				
+//			throw runtimeException;
 			
-			throw runtimeException; 
+			// Console 2):
+//			throw new ScaleOverLimitedException(runtimeException);
+			
+			// Console 3): 
+			System.out.println(runtimeException);
 		}
 		boolean isPass = score >= 60;
 		System.out.printf("Scores: %.2f -> %s\n", score, isPass? "Pass" : "Fail");
@@ -21,7 +44,7 @@ public class RuntimeExceptionDemo {
 		// Notice here - round off to 2nd decimal place
 		printScore(59.125); 
 		printScore(60.514);
-		// throw RuntimeException
+		// RuntimeException
 		printScore(100.001);
 		printScore(60.0); 
 	}	
@@ -29,18 +52,23 @@ public class RuntimeExceptionDemo {
 
 
 /*
-	Console w/o "throw runtimeException": 			
-			Scores: 59.13 -> Fail
-			Scores: 60.51 -> Pass
-			java.lang.RuntimeException: scale over limitedness !!!
-			Scores: 100.00 -> Pass
-			Scores: 60.00 -> Pass
-
+	Console 1) - w/ "throw runtimeException": 			
+				Scores: 59.13 -> Fail
+				Scores: 60.51 -> Pass
+				Exception in thread "main" java.lang.RuntimeException: scale over limitedness !!!
+						
+	Console 2) - w/ "throw new ScaleOverLimitedException(runtimeException)":	
+				Scores: 59.13 -> Fail
+				Scores: 60.51 -> Pass
+				[Number - Engineer - 0002] End-user enters scores over the limitedness of printScore(Double scores) method
+				[Number - ServiceTeam - 0002] My appologies, please enter scores between 60.00 to 100
+				Exception in thread "main" com.ocp.day17.exception.runtimeexception.ScaleOverLimitedException
 	
-	Console w/ "throw runtimeException":	
-			Scores: 59.13 -> Fail
-			Scores: 60.51 -> Pass
-			java.lang.RuntimeException: scale over limitedness !!!
-			Exception in thread "main" java.lang.RuntimeException: scale over limitedness !!!
-		
+	Console 3) - w/o "throw runtimeException" & "throw new ScaleOverLimitedException(runtimeException)":
+				Scores: 59.13 -> Fail
+				Scores: 60.51 -> Pass
+				java.lang.RuntimeException: scale over limitedness !!!
+				Scores: 100.00 -> Pass
+				Scores: 60.00 -> Pass
+
 */
