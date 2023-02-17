4aa578398c633b672887f8953774bbf2faf8deb2
James <JamesCoding888@hotmail.com>
2023-02-17 17:21:47 +0800

    day17

--- JavaOCP20221017/src/main/java/com/ocp/day17/exception/ExceptionDemo2.java
+++ JavaOCP20221017/src/main/java/com/ocp/day17/exception/ExceptionDemo2.java
@@ -11,7 +11,7 @@ public class ExceptionDemo2 {
 		System.out.printf("Scores: %.2f -> %s\n", score, isPass? "Pass" : "Fail");
 	}
 	public static void main(String[] args) 
-		// Console 1): JVM catches the exception because w/o try-catch statement in main method 
+		// Console 1): JVM catches the exception because w/o try-catch and finally statement in main method 
 //		throws Exception   
 	{
 		// Console 2): With try-catch statement in main method
@@ -19,7 +19,12 @@ public class ExceptionDemo2 {
 			printScore(100.0);
 			printScore(-1.005);
 		} catch(Exception e) {
+			// log for engineering team
 			System.out.println("Catch the exception by main method - " + e);
+		} finally {
+			// Following syntax is NOT workable but demo merely, since this project: JavaOCP20221017 doesn't have Server and also no .html/.jsp as well
+			// reDirect to another web-page for end-user to proceed the service continually	
+			System.out.println("redirect:/another-web-page/");
 		}
 	}
 }
@@ -29,10 +34,12 @@ public class ExceptionDemo2 {
 			
 			Scores: 100.00 -> Pass
 			Exception in thread "main" java.lang.Exception: scale over limitedness !!!
+
+
 	
 	Console 2):
 			
 			Scores: 100.00 -> Pass
 			Catch the exception by main method - java.lang.Exception: scale over limitedness !!!
-
+			redirect:/another-web-page/
 */
