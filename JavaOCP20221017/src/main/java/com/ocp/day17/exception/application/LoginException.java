d7cace135aa4e1fb73d35871b2724a4b6959d9a2
James <JamesCoding888@hotmail.com>
2023-02-18 11:39:03 +0800

    day17

--- JavaOCP20221017/src/main/java/com/ocp/day17/exception/application/LoginException.java
+++ JavaOCP20221017/src/main/java/com/ocp/day17/exception/application/LoginException.java
@@ -2,12 +2,28 @@ package com.ocp.day17.exception.application;
 
 public class LoginException extends Exception{
 	
+	/*
+	  	Notice here, the variable naming here:
+		1) Accessed Priviledge shall be Private!
+		2) Cannot change the value of final variable, since variable is constant
+		3) variable naming shall be upper-case letter, because of final variable 
+	*/
+	private final short REPEATED_INCORRECT_PWD = 3;
+	
 	public LoginException(String errorMessage) {
 		super(errorMessage);
-		System.out.println("1");
 	}
 	
+	// Inform end-user to proceed the next step
 	public void how2Do() {
+		++LoginExceptionDemo.lock;
+		if(LoginExceptionDemo.lock == REPEATED_INCORRECT_PWD) {
+			System.out.println("Account is currently locked out and may not be logged on to. "
+						     + "This error when you enter an incorrect password repeatedly. "
+						     + "This error prevents you from signing into your account. "
+						     + "You can fix this issue by contacting Service Team");
+			System.exit(0);
+		}
 		System.out.println("Please enter your password again");
 	}
 }
--- JavaOCP20221017/src/main/java/com/ocp/day17/exception/application/LoginExceptionDemo.java
+++ JavaOCP20221017/src/main/java/com/ocp/day17/exception/application/LoginExceptionDemo.java
@@ -1,39 +1,55 @@
 package com.ocp.day17.exception.application;
 
+import java.util.HashMap;
 import java.util.Map;
-
 public class LoginExceptionDemo  {
 	
-	private static Map<String, String> users;
+	public static short lock = 0;
+	
+	private static Map<String, String> users = new HashMap<>();
 	
 	static {
+		// Following account already stored in Database
 		users.put("James", "#$&(^1WS784");
 		users.put("David", "#&784(^1WS$");
 	}
-	
+
 	public static boolean checkLogin(String username, String password) throws LoginException {
-		String pwd = users.get(password);
-		if(pwd == null) {
+		String pwd = users.get(username);
+		if(pwd == null) {			
 			throw new LoginException("Account Error");			
 		} 
 		if(!pwd.equals(password)) {
 			// Notice here - Hacker can try to guess your password. Do NOT throw Password Error message to end-user !!!
-			// Generally speaking, we will lock account if end-user submits the incorrect password in 3 times. 
+			// Generally speaking, we will lock account if end-user submits incorrect password in 3 times. 
 			throw new LoginException("Password Error");
 		}
-		return false;
+		return true;
 	}
 	
-	public static void main(String[] args) {
-		String username = "James";
-//		String password = "#$&(^1WS784";
-		String password = null;
-		try {
-			boolean isPass = checkLogin(username, password);
-			System.out.println(isPass);
-		} catch(LoginException e) {
-			System.out.println(e.getMessage());
-			e.how2Do();
+	public static void main(String[] args) {			
+		while(true) {				
+			// End-user enters the username but incorrect password on web-page, repeatedly
+			String username = "James";
+			String password = "#$&(^1WS784512";			
+			try {
+				boolean isPass = checkLogin(username, password);			
+				// isPass ? true : LoginException
+				System.out.println(isPass);			
+			} catch(LoginException e) {
+				System.out.println(e.getMessage());
+				e.how2Do();
+			}	
 		}
 	}
 }
+
+/*
+	Console:
+			Password Error
+			Please enter your password again
+			Password Error
+			Please enter your password again
+			Password Error
+			Account is currently locked out and may not be logged on to. This error when you enter an incorrect password repeatedly. This error prevents you from signing into your account. You can fix this issue by contacting Service Team
+*/
