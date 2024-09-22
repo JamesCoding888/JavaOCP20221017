61f4b544ec2dc73bf3c7e8166dd883863807fa25
James Liao <JamesCoding888@hotmail.com>
2024-09-22 14:31:24 +0800

    day17 1. Update the annotation of ExceptionWithTerminal02.java for better readibility

--- JavaOCP20221017/src/main/java/com/ocp/day17/exception/terminate/ExceptionWithTermination02.java
+++ JavaOCP20221017/src/main/java/com/ocp/day17/exception/terminate/ExceptionWithTermination02.java
@@ -1,39 +1,46 @@
 package com.ocp.day17.exception.terminate;
-
 import java.io.BufferedReader; 
 import java.io.FileReader;
 import java.io.IOException;
+
 public class ExceptionWithTermination02 {
 	public static void main(String[] args) {
 		try {
 			/*
-			 
-			   If the path of "helloworld.txt" is Right,
-			   then execute the block code of finally, because of NO IOException being caught.
-			   
-			   If the path of "helloworld.txt" is Wrong, 
-			   then execute the block code of catch statement 
-			   and also skip the block code of finally, because of System.exit(0).	
-			   
-			   Note: Please copy the folder of JavaOCP20221017_NIO from package of "com.ocp.day17.exception.terminate" 
-			         and paste into your desktop.
-			   
-			*/
+			 * Scenario 1: Path is correct (pathInRightDirection)
+			 * - The program reads the file successfully.
+			 * - The finally block will execute after try.
+			 * 
+			 * Scenario 2: Path is incorrect (pathInWrongDirection)
+			 * - An IOException will be thrown.
+			 * - The catch block will handle the exception and call System.exit(0).
+			 * - Since System.exit(0) terminates the program, the finally block will not execute.
+			 * 
+			 * Note:
+			 * - Ensure that the folder "JavaOCP20221017_NIO" exists at the correct location.
+			 * - If needed, copy the folder from the package "com.ocp.day17.exception.terminate"
+			 *   and paste it into your desktop or the correct path.
+			 */
+			
+			// Define both correct and incorrect file paths
+			String pathInRightDirection = "../src/main/java/com/ocp/day17/exception/terminate/JavaOCP20221017_NIO/helloworld.txt";  
+			String pathInWrongDirection = ".../JavaOCP20221017_NIO/helloworld.txt"; // Incorrect path for demonstration
 
-			// Following path is incorrect, please write the correct path of file
-			String pathInRightDirection = ".../JavaOCP20221017_NIO/helloworld.txt";  
-			String pathInWrongDirection = ".../JavaOCP20221017_NIO/helloworld.txt";
+			// Attempt to read from the file
 			BufferedReader br = new BufferedReader(new FileReader(pathInRightDirection));
-			System.out.println(br.readLine());
-			br.close();
+			System.out.println(br.readLine()); // Read and print the first line from the file
+			br.close(); // Close the BufferedReader after use
 		} catch (IOException e) {
+			// Handle any IOException (e.g., file not found, incorrect path)
 			e.printStackTrace();
-			System.exit(0);
+			System.exit(0); // Terminate the program
 		} finally {
+			// This block runs after try/catch unless System.exit(0) is called
 			System.out.println("Exiting the program");
 		}	
 	}
 }
+
 
 /*
 	
